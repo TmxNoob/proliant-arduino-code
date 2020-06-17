ff82f6142880cec7ad06ee51432da13f1de6c3c2
Kaspar Laineste <laineste@cock.li>
2020-06-17 12:34:54 +0300

    Fixed a dumb error

--- controller/controller.ino
+++ controller/controller.ino
@@ -14,6 +14,7 @@ String serialBuffer = "";
 bool stringComplete = false;
 char fanPins[] = {3,5,6,9,10,11};
 char numFans = sizeof(fanPins);
+long lastCommand = 0;
 
 void setupFans() {
   for(char c = 0;c<numFans;c++){
